63b4d55ce409569e8f452fe8f97205b7ac9331c9
raoakif <akifrao@gmail.com>
2023-01-20 04:30:00 +0500

    Added all functionality

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,11 +1,9 @@
 import tIcon from '../assets/images/T.png';
 
+let flag = false;
+let active = -1;
+
 const popupOptions = [
-  {
-    type: 'h1',
-    title: 'Heading 1',
-    description: 'Shortcut: type 1',
-  },
   {
     type: 'h1',
     title: 'Heading 1',
@@ -13,7 +11,72 @@ const popupOptions = [
   },
 ]
 
-const dropDown = (option) => {
+export const filterOptions = (searchInput) => {
+  searchInput = searchInput.slice(1);
+  const options = [...document.querySelectorAll('.popup-options')];
+  options.forEach((option) => {
+    const title = option.querySelector('h4').textContent;
+    option.style.display = title
+      .toLowerCase()
+      .includes(searchInput.toLowerCase())
+      ? 'flex'
+      : 'none';
+  });
+};
+
+export const createInput = (currentInput, currentId) => {
+  const newInput = document.createElement('div');
+  newInput.classList.add('input');
+  newInput.setAttribute('contenteditable', true);
+  newInput.setAttribute('spellcheck', true);
+  newInput.setAttribute('placeholder', "Type '/' for blocks");
+  newInput.setAttribute('id', `${Number(currentId) + 1}`);
+
+  currentInput.insertAdjacentElement('afterend', newInput);
+  newInput.focus();
+
+  return newInput;
+};
+
+export const removePopup = () => {
+  const popup = document.querySelector('.popup');
+  if (popup) {
+    popup.remove();
+    flag = false;
+  }
+};
+
+document.addEventListener('keydown', (e) => {
+  const popOptions = [...document.querySelectorAll('.popup-options')];
+  if (e.key === 'ArrowDown' && active < popOptions.length - 1) {
+    active += 1;
+    popOptions[active].focus();
+  }
+  if (e.key === 'ArrowUp' && active > 0) {
+    active -= 1;
+    popOptions[active].focus();
+  }
+  if (e.key === 'Escape') removePopup();
+});
+
+// remove popup when clicking outside
+document.addEventListener('click', (e) => {
+  if (e.target.closest('.popup')) return;
+  removePopup();
+});
+
+const createTag = (input, tagType) => {
+  input.className = 'input';
+  input.classList.add(tagType);
+  input.textContent = input.textContent.slice(2).trim();
+  tagType === 'p'
+    ? input.setAttribute('placeholder', 'Paragraph')
+    : input.setAttribute('placeholder', `Heading ${tagType}`);
+  input.focus();
+  removePopup();
+};
+
+export const dropDown = (option) => {
   const { title, description } = option;
   return `
     <button class="popup-options" data-type="${option.type}">
@@ -27,7 +90,8 @@ const dropDown = (option) => {
     </button>`;
 };
 
-const popup = (currentInput) => {
+export const createPopupMenu = (currentInput) => {
+  if (flag) return;
   const popupHtml = `
     <div class="popup">
       <div class="popup-heading">
@@ -41,6 +105,15 @@ const popup = (currentInput) => {
     `;
 
   currentInput.insertAdjacentHTML('afterend', popupHtml);
+  flag = true;
 }
 
-export default popup;
+export const selectOption = (input) => {
+  const options = document.querySelectorAll('.popup-options');
+  options.forEach((option) => {
+    option.addEventListener('click', () => {
+      const tagType = option.getAttribute('data-type');
+      createTag(input, tagType);
+    });
+  });
+};
--- src/script.js
+++ src/script.js
@@ -1,16 +1,62 @@
 import './style.css';
-import popup from './modules/popup';
+import { createPopupMenu, selectOption, createInput, filterOptions, removePopup } from './modules/popup';
 
-
-const input = document.querySelector('#input');
-
-const handleInput = () => {
+const handleInput = (input = document.getElementById('input')) => {
   // When someone types ' / ', show the optionsPopup, else don't show anything
   input.addEventListener('input', () => {
     if (input.textContent[0] === '/') {
-      popup(input);
+      createPopupMenu(input);
+      selectOption(input);
+      // filter options
+      filterOptions(input.textContent);
     } else {
-      console.log("Nothing Happened")
+      removePopup();
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    /* If the user presses 'Enter':
+          1. check if the content start with /1, if true, create H1
+          2. check if the content start with /2, if true, create H2
+          3. if the user presses 'Enter' and the input is empty, remove the placeholder
+    */
+
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      const text = input.textContent;
+
+      if (text[0] === '/' && text[1] === '1') createTag(input, 'h1');
+      if (text === '') input.removeAttribute('placeholder');
+
+      const newInput = createInput(input, input.id);
+      handleInput(newInput);
+    }
+
+    /* If the user presses 'Backspace' && the input is empty && the input is not the first one:
+        - Remove the input and focus the previous input
+        - Add the placeholder to the previous <input type="text" />
+        - Put the cursor at the end of the previous input content (used the range object to do that)
+    */
+    if (
+      e.key === 'Backspace' &&
+      input.textContent.length === 0 &&
+      input.id !== '1'
+    ) {
+      const previousInput = input.previousElementSibling;
+      input.remove();
+      previousInput.setAttribute('placeholder', "Type '/' for blocks");
+
+      // Add space to the previous input to make sure the cursor is at the end of the content
+      previousInput.textContent += ' ';
+      previousInput.focus();
+      const range = document.createRange();
+      const sel = window.getSelection();
+      range.setStart(
+        previousInput.childNodes[0] || previousInput,
+        previousInput.textContent.length
+      );
+      sel.removeAllRanges();
+      sel.addRange(range);
     }
   });
 }
